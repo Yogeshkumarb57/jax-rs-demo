9e4a743b78780c79222eed9426c5f506f6bde20f
Yogeshkumar <yogeshkumarb57@gmail.com>
2019-01-10 01:04:29 +0530

    config change

--- src/main/java/com/jaxrs/config/SwaggerConfig.java
+++ src/main/java/com/jaxrs/config/SwaggerConfig.java
@@ -6,17 +6,26 @@ import com.jaxrs.resource.TestRest;
 import io.swagger.jaxrs.config.BeanConfig;
 
 import javax.ws.rs.core.Application;
+import java.net.InetAddress;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SwaggerConfig extends Application {
-
     public SwaggerConfig() {
+
+        String host = null;
+        InetAddress inetAddress = null;
+        try {
+            inetAddress = InetAddress.getLocalHost();
+            host = inetAddress.getHostAddress();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.2");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:7000");
-        beanConfig.setBasePath("/rest");
+        beanConfig.setHost(host + ":7000");
+        beanConfig.setBasePath("/");
         beanConfig.setResourcePackage("io.swagger.resources");
         beanConfig.setScan(true);
     }
--- src/main/java/com/jaxrs/config/SwaggerConfigUsingServlet.java
+++ src/main/java/com/jaxrs/config/SwaggerConfigUsingServlet.java
@@ -1,17 +1,23 @@
 package com.jaxrs.config;
 
-import io.swagger.jaxrs.config.BeanConfig;
-
 public class SwaggerConfigUsingServlet {//extends HttpServlet {
    /* @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
 
+        String host=null;
+        InetAddress inetAddress = null;
+        try{
+           inetAddress =InetAddress.getLocalHost();
+           host = inetAddress.getHostAddress();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.2");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8050");
-        beanConfig.setBasePath("/rest");
+        beanConfig.setHost(host+":7000");
+        beanConfig.setBasePath("/");
         beanConfig.setResourcePackage("io.swagger.resources");
         beanConfig.setScan(true);
     }*/
