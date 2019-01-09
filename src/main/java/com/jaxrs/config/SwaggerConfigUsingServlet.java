39d375dca6682723b45a905e09050aec656db937
Yogeshkumar <yogeshkumarb57@gmail.com>
2019-01-10 02:22:34 +0530

    added embedded tomcat, config change

--- src/main/java/com/jaxrs/config/SwaggerConfigUsingServlet.java
+++ src/main/java/com/jaxrs/config/SwaggerConfigUsingServlet.java
@@ -18,25 +18,23 @@ public class SwaggerConfigUsingServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         Info info = new Info()
-                .title("Swagger Sample App")
-                .description("This is a sample server Petstore server.  You can find out more about Swagger " +
-                        "at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, " +
-                        "you can use the api key `special-key` to test the authorization filters.")
-                .termsOfService("http://swagger.io/terms/")
+                .title("Rest APIs using JAX-RS")
+                .description("Using jersey as implementation and documentation with swagger")
+                .termsOfService("")
                 .contact(new Contact()
-                        .email("apiteam@swagger.io"))
+                        .email("yogeshkumarb57@gmail.com"))
                 .license(new License()
                         .name("Apache 2.0")
                         .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
 
         ServletContext context = config.getServletContext();
         Swagger swagger = new Swagger().info(info);
-        swagger.securityDefinition("api_key", new ApiKeyAuthDefinition("api_key", In.HEADER));
+        /*swagger.securityDefinition("api_key", new ApiKeyAuthDefinition("api_key", In.HEADER));
         swagger.securityDefinition("petstore_auth",
                 new OAuth2Definition()
                         .implicit("http://petstore.swagger.io/api/oauth/dialog")
                         .scope("read:pets", "read your pets")
-                        .scope("write:pets", "modify pets in your account"));
+                        .scope("write:pets", "modify pets in your account"));*/
         new SwaggerContextService().withServletConfig(config).updateSwagger(swagger);
     }
 }
